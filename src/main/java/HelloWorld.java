95f79f70f1264244d75517fb5c4a55493617c64e
Simone <simone2002esposito@gmail.com>
2022-09-30 12:37:45 +0200

    Second Version Hello World & Operations

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 
     public static void main (String[] args){
         System.out.println ("Hello World");
+        int a = 5, b = 8;
+        System.out.println (a + b - 10);
     }
 }
